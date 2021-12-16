45f26d78936ff44a0c026874ca30fe27d990a844
ExequielAndres <42586210+ExequielAndres@users.noreply.github.com>
2021-12-16 12:47:36 -0300

    Update App.py

--- App.py
+++ App.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def  Index():
-    return render_template('index.html')
+def  Hello_World():
+    return 'hello world'
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run()
