107c2599a07cf7ec516feb73019326332515fe95
ExequielAndres <42586210+ExequielAndres@users.noreply.github.com>
2021-12-21 15:16:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def Formulario(producto):
 
 def Correo(correo, producto):
 
-    msg = Message('Solicitud de Compra', sender='tienda.entrehumoos@gmail.com', recipients = [correo, "ventas.entrehumoos@gmaill.com"])
+    msg = Message('Solicitud de Compra', sender='tienda.entrehumoos@gmail.com', recipients = [correo, "ventas.entrehumoos@gmail.com"])
     msg.body = 'Hola, tu solicitud:" '+producto+' " ha sido enviada a Entrehumoos y está siendo procesada. Solicitante: '+correo
     mail.send(msg)
 
